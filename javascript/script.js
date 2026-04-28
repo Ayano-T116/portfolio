1c22dfa3c30ccacaebec0476da11f5cf8fae5974
Ayano-T116 <fdkjy093@gmail.com>
2026-04-28 14:36:18 +0900

    定数定義ファイルを作成

--- javascript/script.js
+++ javascript/script.js
@@ -1,32 +1,5 @@
 
-//作品一覧
-const works = [
-    {
-        workname: '01profile',
-        description: `自己紹介サイトのトップページ`,
-        languages: `HTML/CSS`,
-        text: `保守性能を高めるため、ソースコードを整理しながら記述しました。領域ごとにコメントを追加しました。`
-    },
-    {
-        workname: '02photo',
-        description: `フォト紹介サイトのトップページ`,
-        languages: `HTML/CSS`,
-        text: `class指定を使い、cssの記述を整理しました。`},
-    {
-        workname: '03photo2',
-        description: `フォト紹介サイトのトップページ`,
-        languages: `HTML/CSS`,
-        text: `レスポンシブ対応で、改行し余白を調整する部分を丁寧に記述しました。
-               footerやsectionなど、意味を考慮して適切なタグを使用しました。`},
-    {
-        workname: '04recipe',
-        description: `レシピ紹介サイト(トップページ・詳細ページ)`,
-        languages: `HTML/CSS`,
-        text: `メインの３つの画像はリスト形式で並べました。
-               画像の大きさとリスト要素の大きさが合わない問題がありましたが、
-               リストの横幅を計算式を利用して指定することで解決できました。<br>
-               トップページと詳細ページで共通となる部分は、CSSを共通化し、複数ページで再利用できる構成にしました。`},
-];
+import { works } from './constants.js';
 
 //リスト取得
 const list = document.getElementById('worksList');
@@ -42,7 +15,7 @@ works.forEach(
             </div>
             <div class="listContents">
                 <div><img class="worksImage" src="image/${work.workname}.png" alt="${work.workname}"></div>
-                <div><a class="worksButton" href="works_CodeJump/${work.workname}/" target="_blank">show</a></div>
+                <div><a class="worksButton" href="works_CodeJump/${work.workname}/" target="_blank">show page</a></div>
             </div>
             <div class="listText">
                 <h4>＜使用言語＞</h4>
